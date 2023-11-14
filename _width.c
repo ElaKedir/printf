c2337e9f8335248cbabff3b8cc1e122ef8ba56ab
oyingidie <114517560+oyingidie@users.noreply.github.com>
2023-11-14 22:04:57 +0100

    A thousand and one times wide

--- _width.c
+++ _width.c
@@ -2,12 +2,13 @@
 
 /**
  * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @format: Formatted string in which to print the arguments
+ * @i: Input
+ * @list: List of arguments
  *
- * Return: width.
+ * Return: Width
  */
+
 int get_width(const char *format, int *i, va_list list)
 {
 	int index;
@@ -34,4 +35,3 @@ int get_width(const char *format, int *i, va_list list)
 
 	return (width);
 }
-
