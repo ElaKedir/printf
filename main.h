d628f6ead696d77a20552635b7097e8ae59f1f74
oyingidie <114517560+oyingidie@users.noreply.github.com>
2023-11-15 00:50:12 +0100

    Headers above footers

--- main.h
+++ main.h
@@ -82,4 +82,6 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
+void print_buffer(char buf[], int *buff_ind);
+
 #endif
