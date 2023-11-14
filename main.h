d99ea3628d6131e7d2f665ea44ca15ead2d1b42e
oyingidie <114517560+oyingidie@users.noreply.github.com>
2023-11-14 23:03:49 +0100

    Headers above footers

--- main.h
+++ main.h
@@ -29,12 +29,6 @@ struct fmt
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
 };
-/**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format
- * @fm_t: The associated function
- */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
